db3741289f2cbdfac49b560b967b8e1047330c1d
Alice Fontes <alice.fontes@corp.globo.com>
2017-03-09 13:53:48 -0300

    initial page added

---
+++ app/controllers/closet_controller.rb
@@ -0,0 +1,11 @@
+class ClosetController < ApplicationController
+
+  def show
+    render 'initialpage'
+  end
+
+  def new
+    render 'new'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "closet#show", as: :show
+  get '/new', to: 'closet#new', as: :init
 end
